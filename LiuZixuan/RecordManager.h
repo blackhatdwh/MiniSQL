9126eaea7d2c5841796797620d727b288638576e
刘子旋 <3150104437@zju.edu.cn>
2017-06-30 17:38:20 +0000

    Upload files to 'LiuZixuan'

--- LiuZixuan/RecordManager.h
+++ LiuZixuan/RecordManager.h
@@ -2,29 +2,37 @@
 #define __RECORDMANAGER_H__
 
 #include "BufferManager.h"
-#include "CatalogManager.h"
 #include "RecordManager.h"
+#include "CatalogManager.h"
 #include <iostream>
 #include <vector>
+#include <string>
+#define EMPTY '#'
 
 extern BufferManager buf;
-extern CatalogManager cat;
+extern CatalogManager cam;
 
-struct condition
-{
-	std::string ope1;
-	std::string val1;
-	std::string ope2;
-	std::string val2;
-	Attribute a;
+struct Condition {
+	int column;
+	int ope;
+	std::string value;
 };
-
 
 class RecordManager {
 public:
-	void Select(Attribute &attr, Table &t, condition &cond, std::vector<Tuple> &tuples);
+	void Select(int column, Table &t, condition &cond, std::vector<Tuple> &tuples);
+	void Select_Equal(Table &t, int column_to_compare, std::string value, ::vector<Tuple> &tuples);
+	void Select(Table &t, std::vector<Condition> &cond, std::vector<Tuple> &tuple);
+	void Insert(Tuple &tuple, Table &t);
+	void Delete(Table &t, std::vector<Condition> &cond, std::vector<Tuple> &tuple);
+
 	void Delete();
 	void Insert();
+	friend class IndexManeger;
+	friend class BPlusTree;
+	friend class CatalogManager;
+	friend class RecordManager;
+	friend class BufferManager;
 private:
 
 };
